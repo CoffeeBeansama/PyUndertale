e65803e1f4f7124aaa0e588e0b0482924819aaa7
Francis Aigo <103745801+CoffeeBeansama@users.noreply.github.com>
2023-12-01 12:03:10 +0800

    haha

--- battle.py
+++ battle.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from enum import Enum
 from scene import Scene
+from timer import Timer
 from player import PlayerSoul
 from support import loadSprite
 from eventHandler import EventHandler
@@ -8,6 +9,11 @@ from eventHandler import EventHandler
 class Turn(Enum):
     PlayerTurn = 1
     EnemyTurn = 2
+
+class ButtonData(Enum):
+     Sprite = 1
+     SpriteSelected = 2
+     Position = 3
 
 class Battle(Scene):
     
@@ -27,35 +33,57 @@ class Battle(Scene):
             Turn.EnemyTurn : self.enemyTurn
         }
 
+        self.selectionIndex = 0
+        self.buttonPressedTimer = Timer(200)
+
     def createButtons(self):
         spritePath = "Sprites/UI/"
-        buttonSize = (50,30)
-        self.buttonSprites = {
-            "Fight" : loadSprite(f"{spritePath}FightButton.png",buttonSize),
-            "Act" : loadSprite(f"{spritePath}ActButton.png",buttonSize),
-            "Item" : loadSprite(f"{spritePath}ItemButton.png",buttonSize),
-            "Mercy" : loadSprite(f"{spritePath}MercyButton.png",buttonSize)
+        buttonSize = (140,70)
+
+        self.playerButton = {
+            "Fight": {},
+            "Act": {},
+            "Item": {},
+            "Mercy": {}
         }
 
+        for i,key in enumerate(self.playerButton.keys()):
+             self.playerButton[key][ButtonData.Sprite] = loadSprite(f"{spritePath}{key}Button.png",buttonSize)
+             self.playerButton[key][ButtonData.SpriteSelected] = loadSprite(f"{spritePath}{key}ButtonSelected.png",buttonSize)
+             self.playerButton[key][ButtonData.Position] = (40+(160*i),400)
+
+        self.buttons = [i for i in self.playerButton.values()]
+        
+
+    def handleInput(self):
+        self.eventHandler.handlePlayerInput()
+        self.buttonPressedTimer.update()
+
+        if not self.buttonPressedTimer.activated:
+            if self.eventHandler.pressingRightButton():
+                    self.selectionIndex += 1
+            if self.eventHandler.pressingLeftButton():
+                    self.selectionIndex -= 1
+            self.buttonPressedTimer.activate()
+
+        if self.selectionIndex < 0:
+             self.selectionIndex = 0
+
+        if self.selectionIndex > len(self.buttons) -1:
+             self.selectionIndex = int(len(self.buttons)) - 1
+    
     def playerTurn(self):
-        pass
+        self.handleInput()
+        for index,button in enumerate(self.buttons):
+            self.screen.blit(button[ButtonData.Sprite] if index != self.selectionIndex else button[ButtonData.SpriteSelected],
+                             button[ButtonData.Position])
 
     def enemyTurn(self):
         for sprites in self.visibleSprites:
             self.screen.blit(sprites.sprite,sprites.rect.center)
-    
-    def handleInput(self):
-        self.eventHandler.handlePlayerInput()
 
-        if self.eventHandler.pressingRightButton():
-            pass
-        if self.eventHandler.pressingLeftButton():
-            pass
-
-    def update(self):  
-        self.handleInput()
-      
+    def update(self):      
         getCurrentTurn = self.turns.get(self.currentTurn)
         getCurrentTurn()
-        
+    
         self.player.update()
